41a4f90424441faf3921ab39afefa1a370b22522
x9 <43952633+happy-code-time@users.noreply.github.com>
2022-05-25 06:26:10 +0200

    Update example.node.js

--- example.node.js
+++ example.node.js
@@ -6,18 +6,22 @@ const CompressImagesAll = require('.');
         /**
          * New instance
          */
-        const compress = new CompressImagesAll();
+        await new CompressImagesAll()
         /**
-         * Set source path
+         * Set source path.
+         * 
+         * Default: '' (empty string)
          */
-         compress.setSource(path.join(__dirname, '../../assets/images'));
+        .setSource(path.join(__dirname, 'source'))
         /**
          * Set destination path
+         * 
+         * Default: '' (empty string)
          */
-         compress.setDestination(path.join(__dirname, '../../public/images'));
+        .setDestination(path.join(__dirname, 'public'))
         /**
          * Set allowed files, other files will be ignored.
-         * If an file extendsion added to the array, the file will be ignored
+         * If an file extendsion added to the array, the file will be ignored.
          * 
          * Supported extensions are:
          * 
@@ -38,30 +42,69 @@ const CompressImagesAll = require('.');
          * 
          * type: svg
          * package: gulp-svgmin
+         * 
+         * Default: [] (empty array)
          */
-        compress.setExtensions(['jpg', 'jpeg', 'png', 'gif', 'ico', 'svg']);
+        .setExtensions(['jpg', 'jpeg', 'png', 'gif', 'ico', 'svg'])
         /**
-         * Set cache directory for more performance after the first compilation process are done
+         * Set cache directory for more performance after the first compilation process are done.
          * 
          * If the same directory/image are compiled again, the image are readed from the cache
          * directory.
          * 
          * If an image content has been changed, but the name not, this module will detect this
-         * and compile this image again and replace in cache
+         * and compile this image again and replace in cache.
+         * 
+         * Default: '' (empty string -> ignored)
+         */
+        .setCachedDirectory('/tmp/compress-images-all')
+        /**
+         * If using cached option then you can set custom filename for the txt file.
+         * 
+         * Remember, this filename are used for each process to read OLD cached files and the
+         * checksums to be able to make internal checks. If The name are dynamically and chnages each compile process
+         * the caching functionalitynot available.
          * 
          */
-        compress.setCachedDirectory('/tmp/compress-images-all')
+        .setCacheFilename('images_cache')
+        /**
+         * Enable console output.
+         * 
+         * Default: false
+         */
+        .setDisplayLogging(true)
+        /**
+         * If he cache enabled, then after each compress process
+         * old, unused (in current process) files are removed from the
+         * cache directory.
+         * 
+         * Default: false
+         */
+        .setRemoveUnusedFiles(true)
+        /**
+         * If the destination folder contains current image
+         * you can delete it, before copy it from cache.
+         * 
+         * This feature only available if caching are enabled.
+         * 
+         * Default: false
+         */
+        .setRemoveTargetIfExists(true)
         /**
          * The start function return an Promise
          */
-        await compress
-            .start()
-            .then(() => {
-                console.log("Done");
-            })
-            .catch((e) => {
-                console.error(e);
-            })
-        ;
+        .start()
+        /**
+         * After the promise are resolved
+         */
+        .then(() => {
+            console.log("Done");
+        })
+        /**
+         * On promise catch
+         */
+        .catch((e) => {
+            console.error(e);
+        });
     }
 )();
